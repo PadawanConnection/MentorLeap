e695a83bd2ab9bc462658d4f22e632485711286a
Nicholas Marazzo <designmarz@gmail.com>
2015-07-22 22:41:30 -0700

    working on mongo

--- api/models/User.js
+++ api/models/User.js
@@ -89,6 +89,8 @@ module.exports = {
     var obj = this.toObject();
     delete obj.password;
     delete obj.confirmation;
+    delete obj.encryptedPassword;
+    delete obj._csrf;
     return obj;
    }
   
